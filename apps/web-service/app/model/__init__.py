472816e33c34f11f779b4930f5808598f01fe5ce
Ryan-CN-BJ <maorunjie2025@qq.com>
2026-07-14 20:59:40 +0800

    feat：系统测试

--- apps/web-service/app/model/__init__.py
+++ apps/web-service/app/model/__init__.py
@@ -1,3 +1,5 @@
 from app.model.category import Category
 from app.model.product import Product
 from app.model.sku import Sku
+from app.model.setting import Setting
+from app.model.setting_group import SettingGroup
